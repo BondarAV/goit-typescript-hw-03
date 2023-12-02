32c6e5019c6059edca017bf77ddbe20be654fec0
Andrii Bondar <77549085+BondarAV@users.noreply.github.com>
2023-12-02 02:46:50 +0200

    Completes the tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,51 @@
+class Key {
+  private signature: number = Math.random();
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) this.tenants.push(person);
+  }
+
+  isDoorOpen(): boolean {
+    return this.door;
+  }
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +54,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
